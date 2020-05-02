c89cd843a81c1982e9ac5944f4d126f52e8f231f
NandaSatriaPutra <nsatria993@gmail.com>
2020-05-02 22:06:19 +0700

    menambah data sparepart  melalui post man

--- routes.js
+++ routes.js
@@ -14,4 +14,10 @@ module.exports = function(app){
 
     app.route('/tambahsparepart')
     .post(jsonku.tambahsparepart)
+
+    app.route('/ubahsparepart')
+    .put(jsonku.ubahsparepart);
+
+
+}
 }
